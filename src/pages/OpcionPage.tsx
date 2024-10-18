b85e87485f409ce502cfd0b827e191c531e4838f
edmendezvilla <146900895+edmendezvilla@users.noreply.github.com>
2024-10-17 19:33:41 -0500

    update

---
+++ src/pages/OpcionPage.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Options from '../Components/Options';
+
+const OpcionesPage: React.FC = () => {
+
+  const [characters] = useState<string[]>(['Un caballero', 'Un dragón']);
+  const [scenarios] = useState<string[]>(['un castillo', 'un bosque encantado']);
+  const [events] = useState<string[]>(['rescata a alguien', 'encuentra un tesoro']);
+
+
+  const [character, setCharacter] = useState<string>('');
+  const [scenario, setScenario] = useState<string>('');
+  const [event, setEvent] = useState<string>('');
+
+  return (
+    <div>
+      <h2>Selecciona los elementos de tu historia</h2>
+      <Options
+        characters={characters} 
+        scenarios={scenarios}   
+        events={events}         
+        seleccionarCharacter={setCharacter} 
+        seleccionarScenario={setScenario}  
+        seleccionarEvent={setEvent}        
+      />
+    </div>
+  );
+};
+
+export default OpcionesPage;
