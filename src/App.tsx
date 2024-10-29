2c19e7c1e18162ff98b5b361f44c3d5aeddcfeda
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-10-28 19:02:38 -0500

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import HistoryPage from './pages/HistoryPage'; 
+import HistoryPage from './pages/HistoryPage';
+import Footer from './pages/Footer';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <HistoryPage /> {/* Renderiza la página de generación de historias */}
-    </div>
-  );
+    return (
+        <div>
+            <HistoryPage /> {/* Renderiza la página de generación de historias */}
+            <Footer /> {/* Pie de página */}
+        </div>
+    );
 };
 
 export default App;
---
+++ src/pages/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Instituto Sudamericano. Todos los derechos reservados por Edisson, Alexis y alejandra.</p>
+        </footer>
+    );
+};
+
+export default Footer;
