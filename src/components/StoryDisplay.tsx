6ac0e770d9fe88a6df7350fd9571df9211560035
edmendezvilla <146900895+edmendezvilla@users.noreply.github.com>
2024-11-07 06:36:49 -0500

    .

--- src/components/StoryDisplay.tsx
+++ src/components/StoryDisplay.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 interface Props {
-character: string;
-location: string;
-event: string;
+  character: string;
+  location: string;
+  event: string;
 }
 
 const StoryDisplay: React.FC<Props> = ({ character, location, event }) => {
-return (
-<div>
-<h3>Historia Generada</h3>
-<p>
-    {`Había una vez un ${character} que estaba en ${location} y de repente ${event}.`}
-</p>
-</div>
-);
+  return (
+    <div>
+      <h3>Historia Generada</h3>
+      <p>
+        {`Había una vez un ${character} que estaba en ${location} y de repente ${event}.`}
+      </p>
+    </div>
+  );
 };
 
 export default StoryDisplay;
+
